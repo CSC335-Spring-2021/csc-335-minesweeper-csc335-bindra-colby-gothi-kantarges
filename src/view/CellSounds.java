d63749b44a2ad335a6b81cd3a03626ddaec86726
Prabhkirat Singh Bindra <psbin1721@gmail.com>
2021-05-05 09:39:44 -0700

    optimized explosion responsiveness
    Also removed Collections.reverseOrder() in HighScores. Instead, the
    ScoreEntry comparator was reversed.

--- src/view/CellSounds.java
+++ src/view/CellSounds.java
@@ -1,11 +1,9 @@
 package view;
 
 import java.io.File;
-
 import javafx.scene.media.Media;
-import javafx.scene.media.MediaPlayer;
 
 public class CellSounds {
 
-	static MediaPlayer mediaPlayer = new MediaPlayer(new Media(new File("sounds/explosion.wav").toURI().toString()));
+	static Media explosion = new Media(new File("sounds/explosion.wav").toURI().toString());
 }
